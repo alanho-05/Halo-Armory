82a979028b61119dead307bd5dfebf97d6b64bce
Alan Ho <aho0593@gmail.com>
2023-07-12 04:41:49 +0000

    Completed home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,29 +4,36 @@ import './Home.css';
 export default function Home() {
   return (
     <>
-      <header className="container px-4 my-2 text-white rounded-3 h-50">
+      <header className="container px-4 mt-5 text-white rounded-3 h-50">
         <h1 className="display-5 fw-bold">Welcome to the armory</h1>
         <p class="col-md-8 lead">Your one stop shop for all your halo needs</p>
       </header>
       <div className="container">
-        <div className="row justify-content-between">
+        <div className="row justify-content-center">
+          <h1 className=" mt-4 mb-5 text-center text-white">
+            Product categories
+          </h1>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row justify-content-center">
           <Link
-            className="col-md-4 col-12 weapon-bg"
+            className="d-flex align-items-end col-md-4 col-12 mb-5 mx-2 weapon-bg"
             to="Weapons"
             style={{ textDecoration: 'none' }}>
-            Weapons
+            <h2 className="text-white">Weapons</h2>
           </Link>
           <Link
-            className="col-md-4 col-12 vehicle-bg"
+            className="d-flex align-items-end col-md-4 col-12 mb-5 mx-2 vehicle-bg"
             to="Vehicles"
             style={{ textDecoration: 'none' }}>
-            Vehicles
+            <h2 className="text-white">Vehicles</h2>
           </Link>
           <Link
-            className="col-md-4 col-12 throwable-bg"
+            className="d-flex align-items-end col-md-4 col-12 mb-5 mx-2 throwable-bg"
             to="Throwables"
             style={{ textDecoration: 'none' }}>
-            Throwables
+            <h2 className="text-white">Throwables</h2>
           </Link>
         </div>
       </div>
